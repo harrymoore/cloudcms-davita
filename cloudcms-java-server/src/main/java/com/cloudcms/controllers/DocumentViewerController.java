599ee8a7f696b51c69e81f9b144229c10d35a21e
Harry Moore <harrymoore@yahoo.com>
2021-01-22 16:35:58 -0500

    UI updates

--- cloudcms-java-server/src/main/java/com/cloudcms/controllers/DocumentViewerController.java
+++ cloudcms-java-server/src/main/java/com/cloudcms/controllers/DocumentViewerController.java
@@ -4,12 +4,13 @@
 package com.cloudcms.controllers;
 
 import java.util.List;
+import java.util.Map;
 
 import com.cloudcms.server.CloudCmsDriverBranchNotFoundException;
 import com.cloudcms.server.CloudcmsDriver;
 
+import org.gitana.platform.client.attachment.Attachment;
 import org.gitana.platform.client.node.Node;
-import org.gitana.platform.client.node.NodeImpl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,28 +41,49 @@ public class DocumentViewerController {
             final Model map)
             throws CloudCmsDriverBranchNotFoundException {
 
+        log.debug("getDocument()");
+        
         // retrieve only metadata. not a binary attachment
-        Boolean includeMetadata = Boolean.parseBoolean(metadata);
-
-        log.debug("/index/{} {}", id, map.toString());
+        // Boolean includeMetadata = Boolean.parseBoolean(metadata);
 
         map.addAttribute("filter", filter);
 
+        final Boolean cache = Boolean.parseBoolean(useCache);
+
         // add the list of documents to the model so that an index can be built
-        List<Node> nodes = driver.queryNodesByType(driver.getBranch(branch).getId(), driver.getLocale(), NODE_TYPE, Boolean.parseBoolean(useCache));
-        map.addAttribute("documents", nodes);
+        List<Node> nodes = driver.queryNodesByType(driver.getBranch(branch).getId(), driver.getLocale(), NODE_TYPE, cache);
+        map.addAttribute("indexDocuments", nodes);
 
         // add the requested document, if it exists, to the model
         if (null != id && !id.isEmpty()) {
-            map.addAttribute("document", this.driver.getNodeById(driver.getBranch(branch).getId(), driver.getLocale(), id, Boolean.parseBoolean(useCache)));
+            Node node = driver.getNodeById(driver.getBranch(branch).getId(), driver.getLocale(), id, cache);
+            map.addAttribute("document", node);
+
+            // for each "document" relator item, gather info about the related node and it's "default" attachment
+            List<Map<String,String>> relatedDocuments = (List<Map<String,String>>)node.get("document");
+            for(Map<String,String> doc : relatedDocuments) {
+                log.debug("{} {}", doc.get("id"), doc.get("ref"));
+                Attachment attachment = driver.getNodeAttachmentById(driver.getBranch(branch).getId(), driver.getLocale(), (String)doc.get("id"), "default", cache);
+
+                doc.put("mimetype", attachment.getContentType());
+                doc.put("isVideo", String.valueOf(attachment.getContentType().startsWith("video/")));
+                doc.put("isAudio", String.valueOf(attachment.getContentType().startsWith("audio/")));
+                doc.put("isPdf", String.valueOf(attachment.getContentType().endsWith("/pdf")));
+            }
+
+            map.addAttribute("attachments", relatedDocuments);
+
+            // node.associations(QName.create("a:linked"), Direction.OUTGOING).asList().forEach(association -> {
+            //     map.addAttribute(association.get, attributeValue)
+            // });;
             // log.debug("{}", ((NodeImpl)map.getAttribute("document")).get("title"));
             // log.debug("{}", ((NodeImpl)map.getAttribute("document")).getSystemObject().get("modified_on").get("iso_8601").textValue());
             // log.debug("{}", ((NodeImpl)map.getAttribute("document")).getSystemObject().get("modified_by").textValue());
+
         }
         else
         {
-            map.addAttribute("document", nodes.size() > 0 ? nodes.get(0) : null);
-            // map.addAttribute("document", null);
+            map.addAttribute("document", !nodes.isEmpty() ? nodes.get(0) : null);
         }
 
         return VIEW_NAME;
