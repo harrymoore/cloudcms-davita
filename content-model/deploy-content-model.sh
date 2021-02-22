7c6667737c1822cba8db508bc9f5efa7465a11d7
Harry Moore <harrymoore@yahoo.com>
2021-02-22 17:06:27 -0500

    Include Davita's projects when deploying content model

--- content-model/deploy-content-model.sh
+++ content-model/deploy-content-model.sh
@@ -5,5 +5,11 @@
 
 # use cloudcms-util import feature to upload a local copy of the content model to a cloud cms branch
 # npm install cloudcms-util
+
+# harry's dev projects:
 npx cloudcms-util import -g ../gitana-editorial.json --branch master --all-definitions --folder-path .
 npx cloudcms-util import -g ../gitana-live.json --branch master --all-definitions --folder-path .
+
+# Davita / pixit projects:
+npx cloudcms-util import -g ../gitana-davita-pcomm.json --branch master --all-definitions --folder-path .
+npx cloudcms-util import -g ../gitana-davita-pcomm-live.json --branch master --all-definitions --folder-path .
